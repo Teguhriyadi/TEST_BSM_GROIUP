85a2a63c46533b0e862bb916efa17d9ac527c1fc
Teguhriyadi <ilham.teguh55@gmail.com>
2026-09-19 15:19:43 +0700

    Update Backup Seeder

--- database/seeders/BackupSeeder.php
+++ database/seeders/BackupSeeder.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
-class DatabaseSeeder extends Seeder
+class BackupSeeder extends Seeder
 {
     public function run(): void
     {
